8eff3a127a7bfd428870fc9e7b0d54f73f70a0c2
manish salvi <salvimanish3006@gmail.com>
2020-09-29 20:40:32 +0530

    complaints

--- complaints/urls.py
+++ complaints/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
 
     path('', views.complaints_form, name='complaint_insert'),
-    path('list', views.complaints_list, name='list'),
-    path('<int:id>', views.complaints_form, name='complaint_update'),
-    path('delete/<int:id>', views.complaints_delete, name='complaint_delete')
+    path('list/', views.complaints_list, name='list'),
+    path('<int:id>/', views.complaints_form, name='complaint_update'),
+    path('delete/<int:id>/', views.complaints_delete, name='complaint_delete')
 ]

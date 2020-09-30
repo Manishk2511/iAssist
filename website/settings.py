2079a399c8119924f733f632730bab12e0d3fe29
manish salvi <salvimanish3006@gmail.com>
2020-09-30 13:14:19 +0530

    edit

--- website/settings.py
+++ website/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'iAssist',
         'USER':  'postgres',
         'PASSWORD': 'manish25',
